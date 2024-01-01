b8d05afe4db17665581162138986f4f03b0ad58b
Wemi Moyela <wemymoyela@gmail.com>
2024-01-01 06:53:54 +0100

    switched branch to sinatra

---
+++ server.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'json'
+require_relative 'encryptor'
+require_relative 'decryptor'
+
+get '/' do
+  'Server active!'
+end
+
+get '/encrypt' do
+  content_type :json
+
+  request_payload = JSON.parse(request.body.read)
+
+  input_string = request_payload['text']
+  input_key = request_payload['key']
+
+  encrypted_text = caeser_cypher_encrypt(input_string, input_key)
+
+  { output:encrypted_text, key:input_key }.to_json
+end
+
+get '/decrypt' do
+  content_type :json
+
+  request_payload = JSON.parse(request.body.read)
+
+  input_string = request_payload['text']
+  input_key = request_payload['key']
+
+  decrypted_text = caeser_cypher_decrypt(input_string, input_key)
+  { result: decrypted_text }.to_json
+end
